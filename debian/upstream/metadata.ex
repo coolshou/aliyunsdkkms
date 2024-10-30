2fad3b287c14a345207c7023a403d960e886d356
coolshou <jimmy_yeh@alphanetworks.com>
2024-10-30 11:02:22 +0800

    import 2.16.5

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/aliyunsdkkms/issues
+# Bug-Submit: https://github.com/<user>/aliyunsdkkms/issues/new
+# Changelog: https://github.com/<user>/aliyunsdkkms/blob/master/CHANGES
+# Documentation: https://github.com/<user>/aliyunsdkkms/wiki
+# Repository-Browse: https://github.com/<user>/aliyunsdkkms
+# Repository: https://github.com/<user>/aliyunsdkkms.git
